f411bb78c57b52ef618857a17a7aa879ee6ac4bf
kjhgfdsa1014 <kjhgfdsa1014@gmail.com>
2024-10-25 05:44:20 +0000

    Second last

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 export const GlobleVariable = {
   Backend_url: import.meta.env.VITE_BACKEND_URL,
   React_api: import.meta.env.VITE_STABLE_DEFFUSION_API,
+  // React_api2: import.meta.env.VITE_STABLE_DEFFUSION_API2,
+  // React_api3: import.meta.env.VITE_STABLE_DEFFUSION_API3,
+  // React_api4: import.meta.env.VITE_STABLE_DEFFUSION_API4,
+  // React_api4: import.meta.env.VITE_STABLE_DEFFUSION_API5,
 
 }
 
